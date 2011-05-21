e46f762858f6fe2452550e938ed1b34f8f7b9ce9
chris <parweb@gmail.com>
2011-05-21 16:17:33 +0200

    test connection bdd pagodabox

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
 <?php
 
-echo 'plop';
+if ( $_GET['mysql'] == 'on' ) {
+	
+	mysql_connect( 'localhost:3307', 'amal', 'MNFDXp8a' ) or die ('Could not connect to mysql server.' );
+	mysql_select_db( 'lorilee' );
+	
+}
+elseif ( $_GET['mysqli'] == 'on' ) {
+	
+	$mysqli = new mysqli( '/tmp/mysql/lorilee.sock', 'amal', 'MNFDXp8a', 'lorilee' );
+	
+	echo '<pre>'.__FILE__.' ( '.__LINE__.' ) ';
+		print_r( $mysqli );
+	echo '</pre>';exit;
+	
+}
+elseif ( $_GET['mysqli2'] == 'on' ) {
+	
+	$mysqli = new mysqli( 'localhost:/tmp/mysql/lorilee.sock', 'amal', 'MNFDXp8a', 'lorilee' );
+	
+	echo '<pre>'.__FILE__.' ( '.__LINE__.' ) ';
+		print_r( $mysqli );
+	echo '</pre>';exit;
+	
+}
