9d67661a2f43855123e3616c698c88ca0ed9c248
chris <parweb@gmail.com>
2011-05-21 16:38:56 +0200

    retest avec mysql_connect

--- index.php
+++ index.php
@@ -1,20 +1,11 @@
 <?php
 
-//mysql_connect( 'localhost:3307', 'amal', 'MNFDXp8a' ) or die ('Could not connect to mysql server.' );
-//mysql_select_db( 'lorilee' );
+$link = mysql_connect('localhost:/tmp/mysql/lorilee.sock', 'amal', 'MNFDXp8a');
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+echo 'Connected successfully';
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////
+mysql_select_db( 'lorilee' );
 
-$mysqli = new mysqli( '/tmp/mysql/lorilee.sock', 'amal', 'MNFDXp8a', 'lorilee' );
-
-echo '<pre>'.__FILE__.' ( '.__LINE__.' ) ';
-	print_r( $mysqli );
-echo '</pre>';
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-$mysqli = new mysqli( 'localhost:/tmp/mysql/lorilee.sock', 'amal', 'MNFDXp8a', 'lorilee' );
-
-echo '<pre>'.__FILE__.' ( '.__LINE__.' ) ';
-	print_r( $mysqli );
-echo '</pre>';
+mysql_close($link);
